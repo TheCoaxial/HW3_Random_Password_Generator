410fa26dffd685526da772158bf745ebf01936fe
Coaxial <thecoaxial@gmail.com>
2020-10-30 17:56:03 -0400

    Added solution for users entering anything other than a number in first prompt with parseInt and isNaN

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ var generateBtn = document.querySelector("#generate");
 
 var len = prompt("Please choose a password length between 8 and 128 characters.");
 
+parseInt(len);
 
 while(len === null){ len = 0;}
 
-while (len < 8 || len > 128){ len = prompt("Please choose a password length between 8 and 128 characters."); }
+while (len < 8 || len > 129 || isNaN(len)){ len = prompt("Please choose a password length between 8 and 128 characters."); }
   
-var testInt = 0;
-
 var lowerCase = prompt("Would you like it to contain lowercase letters? yes/no");
 
 var upperCase = prompt("Would you like it to contain uppercase letters? yes/no");
